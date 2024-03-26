2e3048baadfcec09af31edb7976ad54174d2237a
aditya <yendralwaraditya@gmail.com>
2024-03-26 10:24:01 +0530

    new file

---
+++ Stacks/infix_to_prefix.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void infixToPrefix(string str)
+{
+    stack<char> st;
+    string prefix;
+
+    cout << prefix << endl;
+}
+
+int main()
+{
+
+    return 0;
+}
